8e43621fabecbd64b23de08074b925b6e2439f73
mvkh <kholodov_m@yahoo.ca>
2025-07-16 23:05:56 -0500

    Formatting

--- sdb/test/tests.cpp
+++ sdb/test/tests.cpp
@@ -29,20 +29,24 @@ namespace
         return data[index_of_status_indicator];
     }
 
-    std::int64_t get_section_load_bias(std::filesystem::path path, Elf64_Addr file_address) {
+    std::int64_t get_section_load_bias(std::filesystem::path path, Elf64_Addr file_address) 
+    {
         auto command = std::string("readelf -WS ") + path.string();
         auto pipe = popen(command.c_str(), "r");
 
         std::regex text_regex(R"(PROGBITS\s+(\w+)\s+(\w+)\s+(\w+))");
         char* line = nullptr;
         std::size_t len = 0;
-        while (getline(&line, &len, pipe) != -1) {
+        while (getline(&line, &len, pipe) != -1) 
+        {
             std::cmatch groups;
-            if (std::regex_search(line, groups, text_regex)) {
+            if (std::regex_search(line, groups, text_regex)) 
+            {
                 auto address = std::stol(groups[1], nullptr, 16);
                 auto offset = std::stol(groups[2], nullptr, 16);
                 auto size = std::stol(groups[3], nullptr, 16);
-                if (address <= file_address and file_address < (address + size)) {
+                if (address <= file_address and file_address < (address + size)) 
+                {
                     free(line);
                     pclose(pipe);
                     return address - offset;
@@ -55,7 +59,8 @@ namespace
         sdb::error::send("Could not find section load bias");
     }
 
-    std::int64_t get_entry_point_offset(std::filesystem::path path) {
+    std::int64_t get_entry_point_offset(std::filesystem::path path) 
+    {
         std::ifstream elf_file(path);
 
         Elf64_Ehdr header;
@@ -65,16 +70,19 @@ namespace
         return entry_file_address - get_section_load_bias(path, entry_file_address);
     }
 
-    virt_addr get_load_address(pid_t pid, std::int64_t offset) {
+    virt_addr get_load_address(pid_t pid, std::int64_t offset) 
+    {
         std::ifstream maps("/proc/" + std::to_string(pid) + "/maps");
         std::regex map_regex(R"((\w+)-\w+ ..(.). (\w+))");
 
         std::string data;
-        while (std::getline(maps, data)) {
+        while (std::getline(maps, data)) 
+        {
             std::smatch groups;
             std::regex_search(data, groups, map_regex);
 
-            if (groups[2] == 'x') {
+            if (groups[2] == 'x') 
+            {
                 auto low_range = std::stol(groups[1], nullptr, 16);
                 auto file_offset = std::stol(groups[3], nullptr, 16);
                 return virt_addr(offset - file_offset + low_range);
