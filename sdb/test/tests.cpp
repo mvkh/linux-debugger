dc307ad9a4cedf56bb57f5c9f6315ea1a1d01dc9
mvkh <kholodov_m@yahoo.ca>
2025-07-21 21:23:04 -0500

    Add basic memory write test

--- sdb/test/tests.cpp
+++ sdb/test/tests.cpp
@@ -377,4 +377,16 @@ TEST_CASE("Reading and writing memory works", "[memory]")
     auto data_vec = proc->read_memory(virt_addr{a_pointer}, 8);
     auto data = from_bytes<std::uint64_t>(data_vec.data());
     REQUIRE(data == 0xcafecafe);
+
+    proc->resume();
+    proc->wait_on_signal();
+
+    auto b_pointer = from_bytes<std::uint64_t>(channel.read().data());
+    proc->write_memory(virt_addr{b_pointer}, {as_bytes("Hello, sdb!"), 12});
+
+    proc->resume();
+    proc->wait_on_signal();
+
+    auto read = channel.read();
+    REQUIRE(to_string_view(read) == "Hello, sdb!");
 }
