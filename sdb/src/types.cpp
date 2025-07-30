f4a0b0fdb52e037707e93478d3a9c557d0025afc
mvkh <kholodov_m@yahoo.ca>
2025-07-29 22:02:38 -0500

    Fix typo

--- sdb/src/types.cpp
+++ sdb/src/types.cpp
@@ -14,5 +14,5 @@ sdb::file_addr sdb::virt_addr::to_file_addr(const elf& obj) const
 {
     auto section = obj.get_section_containing_address(*this);
     if (!section) return file_addr{};
-    return file_addr{ obj, addr_ - obj_.load_bias().addr() };
+    return file_addr{ obj, addr_ - obj.load_bias().addr() };
 }
