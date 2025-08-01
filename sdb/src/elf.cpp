b1a2e32ff8af29e208e0e7682fd14632fa2668f9
mvkh <kholodov_m@yahoo.ca>
2025-08-01 18:05:13 -0500

    Add missing class reference

--- sdb/src/elf.cpp
+++ sdb/src/elf.cpp
@@ -40,7 +40,7 @@ sdb::elf::elf(const std::filesystem::path& path)
     parse_symbol_table();
     build_symbol_maps();
 
-    dwarf_ = std::make_unique(*this);
+    dwarf_ = std::make_unique<dwarf>(*this);
 }
 
 sdb::elf::~elf()
