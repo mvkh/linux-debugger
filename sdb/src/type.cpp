042b929dc838cb44d0b72fdf0b2e130a445f13af
mvkh <kholodov_m@yahoo.ca>
2025-08-11 23:48:29 -0500

    Fix dependency

--- sdb/src/type.cpp
+++ sdb/src/type.cpp
@@ -138,6 +138,26 @@ namespace
         }
     }
 
+    sdb::parameter_class merge_parameter_classes(sdb::parameter_class lhs, sdb::parameter_class rhs)
+    {
+        using namespace sdb;
+
+        if (lhs == rhs) return lhs;
+
+        if (lhs == parameter_class::no_class) return rhs;
+        if (rhs == parameter_class::no_class) return lhs;
+
+        if ((lhs == parameter_class::memory) || (rhs == parameter_class::memory)) return parameter_class::memory;
+
+        if ((lhs == parameter_class::integer) || (rhs == parameter_class::integer)) return parameter_class::integer;
+
+        if ((lhs == parameter_class::x87) || (lhs == parameter_class::x87up) || (lhs == parameter_class::complex_x87) ||
+            (rhs == parameter_class::x87) || (rhs == parameter_class::x87up) || (rhs == parameter_class::complex_x87))
+            return parameter_class::memory;
+
+        return parameter_class::sse;
+    }
+
     void classify_class_field(const sdb::type& type, const sdb::die& field, std::array<sdb::parameter_class, 2>& classes, int bit_offset)
     {
         auto bitfield_info = field.get_bitfield_information(type.byte_size());
@@ -192,26 +212,6 @@ namespace
             classes[0] = classes[1] = sdb::parameter_class::memory;
 
         return classes;
-    }
-
-    sdb::parameter_class merge_parameter_classes(sdb::parameter_class lhs, sdb::parameter_class rhs)
-    {
-        using namespace sdb;
-
-        if (lhs == rhs) return lhs;
-
-        if (lhs == parameter_class::no_class) return rhs;
-        if (rhs == parameter_class::no_class) return lhs;
-
-        if ((lhs == parameter_class::memory) || (rhs == parameter_class::memory)) return parameter_class::memory;
-
-        if ((lhs == parameter_class::integer) || (rhs == parameter_class::integer)) return parameter_class::integer;
-
-        if ((lhs == parameter_class::x87) || (lhs == parameter_class::x87up) || (lhs == parameter_class::complex_x87) ||
-            (rhs == parameter_class::x87) || (rhs == parameter_class::x87up) || (rhs == parameter_class::complex_x87))
-            return parameter_class::memory;
-
-        return parameter_class::sse;
     }
 
     bool is_destructor(const sdb::die& func)
